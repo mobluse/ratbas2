bf78caee1b3d10e50f63ec2feca780c421a9b2c6
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-10-20 01:15:00 +0200

    Create test.r

---
+++ test.r
@@ -0,0 +1,22 @@
+x=1; y=2
+if (x == y)
+	print "Wrong, x != y"
+else if(x > y)
+	print "Also wrong, x < y"
+else
+	print "Ok!"
+x=1
+# Here is a comment
+while (x < 10) {
+	if(y != 2) break
+	if(y != 2) next
+	print "x = "`x # backtick instead of semicolon
+	x=x+1
+}
+# REM A BASIC comment
+repeat
+	x=x-1
+until (x == 0)
+for (x=0; x < 10; x=x+1)
+	print "x = "`x
+end
